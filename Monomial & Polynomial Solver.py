378f8d10364850df282dab206909d4040c492cb0
gsullivan18 <gssullivan@umass.edu>
2019-09-06 11:32:42 -0400

    hw1 part 2

--- Monomial & Polynomial Solver.py
+++ Monomial & Polynomial Solver.py
@@ -1,21 +1,23 @@
 class Solver:
-    def __init__(self, a, b, c):
+    def __init__(self, a, b, c): #method used to initialize attributes of class
         self.a = a
         self.b = b
         self.c = c
 
     def m_solver(self):
-        m_result = (float(self.c) + float(self.b))/float(self.a)
+        m_result = (self.c + self.b)/(self.a) #solves monomial algebraically by adding b to c and dividing by a
         return m_result
 
-    #def p_solver(self, a, b,c):
-      #  p_result = ((float(self.c**2)-float(self.b))/float(self.a)
-       # return p_result
+    def p_solver(self, a, b,c):
+        p_result = (self.c**2-self.b)/self.a #solves polynomial agebraically by subtracting b from c squard and dividing by a
+        return p_result
 
-num_input = input("Enter values for a, b, and c in ax-b=c: ").split()
-num1, num2, num3 = map(float, num_input)
-x = Solver(num1,num2,num3)
-calcmono=x.m_solver()
-print(calcmono)
+num_input = input("Enter values for a, b, and c in ax-b=c: ").split() #user input for values a, b and c
+num1, num2, num3 = map(float, num_input) #changes values into floats
 
-#calcpoly=Solver.p_solver(num1,num2,num3)
+solve = Solver(num1,num2,num3) #initialize instance of class
+calcmono=solve.m_solver() #instance of class uses m_solver method
+print("For ax-b=c, x=" + str(calcmono)) #displays answer for monomial
+
+calcpoly=solve.p_solver(num1,num2,num3) #instance of class uses p_solver method
+print("For (ax+b)^1/2=c, x=" +str(calcpoly)) #displays answer for polynomial
